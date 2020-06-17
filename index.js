6ab1b9e9345507671a41d075640f931bae27960f
chenwengao1 <chenwengao@jd.com>
2020-06-17 11:40:23 +0800

    d

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const server=require('cwg-pre-loader/simple-server')
 
 module.exports=(opt)=>{
   const {init_dir, port, safedir, shell, upload, disabledelete}=opt
-  const AESKey=Math.random().toString(36)+Date.now().toString(36)
+  const AESKey=(Math.random().toString(36)+Date.now().toString(36)).match(/.{16}$/)[0]
+  const AESKeyIV=(Math.random().toString(36)+Date.now().toString(36)).match(/.{16}$/)[0]
   const shutdown=server({
     dir: __dirname+'/code',
     listen: port,
@@ -13,6 +14,8 @@ module.exports=(opt)=>{
       UPLOAD: upload,
       DELETE: !disabledelete,
       AESKey,
+      AESKeyIV,
+      Buffer,
     },
     index: 'index.chtml',
     ext: /\.(html|cjs|chtml)$/,
